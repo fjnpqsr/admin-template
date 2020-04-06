79fc350832290c234efa627bb9f8943b59b57d38
fjnpqsr <your@email.com>
2020-04-06 12:33:37 +0800

    chang login router change handler

--- src/models/login.js
+++ src/models/login.js
@@ -1,7 +1,6 @@
 import { login } from '../services/login'
 import { message } from 'antd'
 import { app, setAuthorization } from '../core'
-import * as routerRedux from 'connected-react-router'
 
 const USER_INFO_CONTEXT_NAME = 'userInfo'
 
@@ -11,7 +10,7 @@ export default {
     submitting: false
   },
   effects: {
-    * login ({ payload = {} }, { put, call }) {
+    * login ({ payload = {}, history }, { put, call }) {
       try {
         yield put({ type: 'update', payload: { submitting: true } })
         const data = yield call(login, payload)
@@ -19,18 +18,18 @@ export default {
         yield put({ type: 'update', payload: { submitting: false } })
         app.context.update({ [USER_INFO_CONTEXT_NAME]: user })
         setAuthorization('member')
-        yield put(routerRedux.push('/'))
+        yield put(history.push('/'))
       } catch (e) {
         yield put({ type: 'update', payload: { submitting: false } })
         message.destroy()
         message.error('账号或密码错误')
       }
     },
-    * exit ({ payload = {} }, { put, call, ...props }) {
+    * exit ({ payload = {}, history }, { put, call, ...props }) {
       try {
         app.context.remove(USER_INFO_CONTEXT_NAME)
         setAuthorization()
-        yield put(routerRedux.push('/login'))
+        yield put(history.push('/login'))
       } catch (e) {
         console.log(e)
         message.destroy()
