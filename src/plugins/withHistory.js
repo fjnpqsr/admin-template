00ac4954b6eeb83c48a7a9481e4d3d7ec61c1f56
fjnpqsr <your@email.com>
2020-04-06 12:32:05 +0800

    commit

--- src/plugins/withHistory.js
+++ src/plugins/withHistory.js
@@ -2,7 +2,8 @@ import * as routerRedux from 'connected-react-router'
 
 function withHistory () {
   function onEffect (effect, model, actionType) {
-    return function * (...args) {
+    return function * (...args) { // [action, ...others]
+      // cant inject history into second args, so, redefine action with history
       args[0].history = routerRedux
       yield effect(...args)
     }
