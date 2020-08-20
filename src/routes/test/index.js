7f9bc75a166782642f02f3ca3656d094e2fb2096
fjnpqsr <345239925@qq.com>
2020-08-20 16:17:04 +0800

    test:
    try use useReducer replace redux

--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
-// import Dazzle from '@qsr/dazzle'
+import useReduxs from './useReducer'
 
 const TestPage = () => {
+  const {store, dispatch} = useReduxs({name:'reduxs', user:{age: 12}})
+  console.log({store})
   return (
     <div>
       test page
-      {/* <Dazzle /> */}
+      <p>
+        <button
+          onClick={() => {
+            dispatch({type: 'user/info.name', payload: 'sharon'})
+          }}
+        >change user scope</button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({type: 'prod/info.name', payload: 'reduxs'})
+          }}
+        >change prod scope</button>
+      </p>
+      <p>
+        <button>change test scope</button>
+      </p>
     </div>
   )
 }
---
+++ src/routes/test/useReducer.js
@@ -0,0 +1,48 @@
+import {useReducer} from 'react'
+
+
+const SCOPES_SPLITER = '.'
+const defaultState = {
+}
+
+const writeDeepProps = function (obj, path, value) {
+  const state = {};
+
+  const _path = path.split(SCOPES_SPLITER);
+  console.log(path)
+  const handleArr = [state].concat(_path);
+
+  handleArr.reduce((root, current, index) => {
+    console.log({root})
+    if (index !== handleArr.length -1) {
+      if (!root[current]) {
+        root[current] = {}
+      }
+      return root[current]
+    } else {
+      return root[current] = value
+    }
+  });
+
+  return state
+}
+
+// type: 'user/username'
+// type: 'user/info.age'
+const updateState = (state, {type, payload}) => {
+  const [modal, modalScope] =  type.split('/')
+  const nextState = {...state}
+  if (!nextState[modal]) {
+    nextState[modal] = {}
+  }
+  nextState[modal] = {...nextState[modal], ...writeDeepProps(nextState[modal], modalScope, payload)}
+  return nextState
+}
+export default function (initState = defaultState) {
+  const [store, dispatch] = useReducer(updateState, initState );
+
+  return {
+    dispatch,
+    store
+  }
+}
