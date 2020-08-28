84e631bc97d2cb012f51658abdcead0112bdcbb8
fjnpqsr <345239925@qq.com>
2020-08-28 09:06:07 +0800

    feat:
    remove schedulingTableComponents

--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -1,27 +1,10 @@
 import React from 'react'
-import SchedulingTable from './SchedulingTable'
 
 const TestPage = (props) => {
 
-  const data = [
-    {
-      "name": "小学一年级",
-      "data": ["未", "已", "", "未"]
-    },
-    {
-      "name": "小学二年级",
-      "data": ["未", "已", "", "未"]
-    }
-  ]
-
-  const formatData = data.map(item => ([item.name].concat(item.data)))
-
   return (
     <div>
-      <SchedulingTable
-        classLength={20}
-        data={formatData}
-      />
+      test
     </div>
   )
 }
